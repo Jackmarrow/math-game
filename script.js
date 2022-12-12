a2b1257397b7e94141e5a5deba468ecad8e82722
jackmarrow <jackmarrow06@gmail.com>
2022-12-12 17:45:35 +0100

    math-game

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-const answer = document.getElementById("answer").value;
+const answer = document.getElementById("answer");
 const question = document.getElementById("question");
-const score = document.getElementById("score");
-let num1 = Math.ceil(Math.random() * 10);
-let num2 = Math.ceil(Math.random() * 10);
-function checkAnswer(){
-    if(answer === num1 * num2){
-        question.innerText = `What is ${num1} multiply by ${num2}`
-    }
+const newScore = document.getElementById("score");
+
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+const correctAnswer = num1 * num2;
+
+let score = JSON.parse(sessionStorage.getItem("score"));
+
+question.innerText = `What is ${num1} multiply by ${num2} ?`;
+
+if(!score){
+    score = 0;
 }
 
+newScore.innerText = `score: ${score}`;
+
+if(score >= 0){
+    newScore.style.color = 'green';
+}
+
+else{
+    newScore.style.color = 'red';
+}
+
+function checkAnswer(){
+    if(parseInt(answer.value) === correctAnswer){
+        score++
+        updateScore();
+    }
+    else{
+        score--
+        console.log(score);
+        updateScore();
+    }
+
+}
+
+function updateScore(){
+    sessionStorage.setItem("score",JSON.stringify(score))
+}
 
